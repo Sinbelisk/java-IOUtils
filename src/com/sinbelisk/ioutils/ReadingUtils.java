1949564f92a509fb805f5bab22e89c707b72cb0d
lPhiNix <pablomargamer05@gmail.com>
2024-10-12 11:02:29 +0200

    Folder File Added

--- src/com/sinbelisk/ioutils/ReadingUtils.java
+++ src/com/sinbelisk/ioutils/ReadingUtils.java
@@ -1,4 +1,5 @@
 package com.sinbelisk.ioutils;
 
 public class ReadingUtils {
+
 }
--- src/com/sinbelisk/ioutils/WritingUtils.java
+++ src/com/sinbelisk/ioutils/WritingUtils.java
@@ -1,4 +1,15 @@
 package com.sinbelisk.ioutils;
 
+import java.io.*;
+
 public class WritingUtils {
+    public static void writeLineInTxtFile(File file, String line, boolean append) throws IOException {
+        if (!file.exists()) {
+            throw new FileNotFoundException();
+        }
+
+        try (BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(file, append))) {
+            bufferedWriter.write(line);
+        }
+    }
 }
