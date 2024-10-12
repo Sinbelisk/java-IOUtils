158069df7dd722d988b337dbace0d84c80aa8355
Sinbelisk <rafjimray21y@gmail.com>
2024-10-12 12:02:26 +0200

    añadido metodos para hacer operaciones con lineas de un fichero

--- src/com/sinbelisk/ioutils/CommonUtils.java
+++ src/com/sinbelisk/ioutils/CommonUtils.java
@@ -6,13 +6,5 @@ import java.io.FileReader;
 import java.io.IOException;
 
 public class CommonUtils {
-    public int getFileLines(File file) throws IOException {
-        int lines = 0;
 
-        try(BufferedReader reader = new BufferedReader(new FileReader(file))){
-            while (reader.readLine() != null) lines++;
-        }
-
-        return lines;
-    }
 }
