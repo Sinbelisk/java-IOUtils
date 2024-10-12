9dec94dfc9d4cce56eb70746498728686fa1c280
Sinbelisk <rafjimray21y@gmail.com>
2024-10-12 11:12:13 +0200

    creado metodo para leer las lineas de un archivo

--- src/com/sinbelisk/ioutils/CommonUtils.java
+++ src/com/sinbelisk/ioutils/CommonUtils.java
@@ -1,4 +1,18 @@
 package com.sinbelisk.ioutils;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+
 public class CommonUtils {
+    public int getFileLines(File file) throws IOException {
+        int lines = 0;
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(file))){
+            while (reader.readLine() != null) lines++;
+        }
+
+        return lines;
+    }
 }
--- src/com/sinbelisk/ioutils/ReadingUtils.java
+++ src/com/sinbelisk/ioutils/ReadingUtils.java
@@ -1,4 +1,22 @@
 package com.sinbelisk.ioutils;
 
+import java.io.*;
+
 public class ReadingUtils {
+
+    public String readFileLines(File file) throws IOException {
+        StringBuilder fileTextBuilder = new StringBuilder();
+
+        try(BufferedReader reader = new BufferedReader(new FileReader(file))){
+
+            String bufferLine;
+
+            while((bufferLine = reader.readLine()) != null){
+                fileTextBuilder.append(bufferLine);
+                fileTextBuilder.append("\n");
+            }
+        }
+
+        return fileTextBuilder.toString();
+    }
 }
