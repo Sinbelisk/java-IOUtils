ad2ca7b64d46d1941208c10d6e87bfb127025c33
Sinbelisk <rafjimray21y@gmail.com>
2024-10-12 12:36:29 +0200

    extraido un metodo para recoger el texto de un buffered reader para simplficar la sobrecarga de metodos

--- src/com/sinbelisk/ioutils/CommonUtils.java
+++ src/com/sinbelisk/ioutils/CommonUtils.java
@@ -1,10 +1,15 @@
 package com.sinbelisk.ioutils;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
+import java.nio.charset.Charset;
 
 public class CommonUtils {
+    public static BufferedReader getBufferedReader(File file) throws IOException{
+        return new BufferedReader(new FileReader(file));
+    }
 
+    public static BufferedReader getBufferedReaderStream(File file, Charset charset) throws IOException {
+        if (charset == null) charset = Charset.defaultCharset();
+        return new BufferedReader(new InputStreamReader(new FileInputStream(file), charset));
+    }
 }
--- src/com/sinbelisk/ioutils/ReadingUtils.java
+++ src/com/sinbelisk/ioutils/ReadingUtils.java
@@ -1,69 +1,66 @@
 package com.sinbelisk.ioutils;
 
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
 import java.nio.charset.Charset;
 
 public class ReadingUtils {
     private StringBuilder fileTextBuilder;
-    public String readAndGetFileLines(File file) throws IOException {
+
+    private String readAndGetTextFromBuffer(BufferedReader reader) throws IOException {
         fileTextBuilder = getStringBuilder();
+        String bufferLine;
 
-        try(BufferedReader reader = getBufferedReader(file)){
-
-            String bufferLine;
-
-            while((bufferLine = reader.readLine()) != null){
-                fileTextBuilder.append(bufferLine);
-                fileTextBuilder.append("\n");
-            }
-        }
-        return fileTextBuilder.toString();
-    }
-
-    public String readAndGetFileLine(File file, Charset encoding) throws IOException {
-        fileTextBuilder = getStringBuilder();
-
-        try(BufferedReader reader = new BufferedReader(new FileReader(file, encoding))){
-            String bufferLine;
-
-            while((bufferLine = reader.readLine()) != null){
-                fileTextBuilder.append(bufferLine);
-                fileTextBuilder.append("\n");
-            }
+        while ((bufferLine = reader.readLine()) != null) {
+            fileTextBuilder.append(bufferLine);
+            fileTextBuilder.append("\n");
         }
 
         return fileTextBuilder.toString();
     }
+
+    public String readAndGetFileText(File file) throws IOException {
+        try (BufferedReader reader = CommonUtils.getBufferedReader((file))) {
+            return readAndGetTextFromBuffer(reader);
+        }
+    }
+
+    public String readAndGetFileText(File file, Charset encoding) throws IOException {
+        try (BufferedReader reader = CommonUtils.getBufferedReaderStream(file, encoding)) {
+            return readAndGetTextFromBuffer(reader);
+        }
+    }
+
     public int getFileLines(File file) throws IOException {
         int lines = 0;
+        try (BufferedReader reader = CommonUtils.getBufferedReaderStream(file, null)) {
+            while (reader.readLine() != null) {
+                lines++;
+            }
+        }
         return lines;
     }
+
     public String getFileLine(File file, int line) throws IOException {
         String fileTextLine = "";
 
-        try(BufferedReader reader = getBufferedReaderStream(file, null)){
+        try (BufferedReader reader = CommonUtils.getBufferedReaderStream(file, null)) {
             int currentLine = 0;
-            while ((fileTextLine = reader.readLine()) != null){
-                if(currentLine == line) return fileTextLine;
+            while ((fileTextLine = reader.readLine()) != null) {
+                if (currentLine == line) return fileTextLine;
                 currentLine++;
             }
         }
-
         return null;
     }
-    public int getFileLineCharCount(File file, int line) throws IOException{
+
+    public int getFileLineCharCount(File file, int line) throws IOException {
         return getFileLine(file, line).length();
     }
 
-    private StringBuilder getStringBuilder(){
+    private StringBuilder getStringBuilder() {
         return new StringBuilder();
     }
-    private BufferedReader getBufferedReader(File file) throws IOException{
-        return new BufferedReader(new FileReader(file));
-    }
 
-    private BufferedReader getBufferedReaderStream(File file, Charset charset) throws IOException {
-        if (charset == null) charset = Charset.defaultCharset();
-        return new BufferedReader(new InputStreamReader(new FileInputStream(file), charset));
-    }
 }
