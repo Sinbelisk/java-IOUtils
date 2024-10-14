a06924156afaaffbb1a457f3576ce4fcf52e0c98
lPhiNix <pablomargamer05@gmail.com>
2024-10-14 15:09:15 +0200

    Pull Commit

--- src/com/sinbelisk/ioutils/TEST.java
+++ src/com/sinbelisk/ioutils/TEST.java
@@ -9,8 +9,6 @@ public class TEST {
     // Clase para probar el funcionamiento de la liberia
     public static final String PATH = "files/";
     public static void main(String[] args) throws IOException {
-        File file = new File(PATH + "file.txt");
-        WritingUtils.writeTextLineInMemoryPointer(file, "hola", 2, StandardCharsets.UTF_8);
-
+        FileUtils.copiarArchivoBinario(PATH + "marble_bricks.png", PATH + "marble_bricksCopy.png");
     }
 }
