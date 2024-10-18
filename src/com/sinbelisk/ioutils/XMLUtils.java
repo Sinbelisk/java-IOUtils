4a1cbbf16dac353eaa6232fa6602029e88643297
Rafael <rafjimray21y@gmail.com>
2024-10-18 17:42:23 +0200

    añadido XMLUtils

---
+++ src/com/sinbelisk/ioutils/XMLUtils.java
@@ -0,0 +1,20 @@
+package com.sinbelisk.ioutils;
+
+import org.w3c.dom.Document;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+
+public class XMLUtils {
+
+    public void CreateXML(){
+
+    }
+
+    public Document getNewDocument() throws ParserConfigurationException {
+        DocumentBuilderFactory dbf = DocumentBuilderFactory.newInstance();
+        DocumentBuilder db = dbf.newDocumentBuilder();
+        return db.newDocument();
+    }
+}
